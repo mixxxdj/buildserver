a7cc54569132028f3f087e535250d2430568c518
RJ Ryan <rryan@mixxx.org>
2012-12-13 14:25:51 -0500

    Doh.

--- scripts/linux-ubuntu/setup-buildslave.sh
+++ scripts/linux-ubuntu/setup-buildslave.sh
@@ -9,8 +9,8 @@ sudo apt-get install bzr emacs build-essential
 
 # Enable passwordless sudo
 #echo "WARNING: Enabling passwordless sudo acess for $USER."
-#sudo su -c "echo \"# $USER is given passwordless access to pbuilder and debsign for buildbot purposes.\" >> /etc/sudoers"
-#sudo su -c "echo \"$USER    ALL=(ALL) NOPASSWD: SETENV: /usr/bin/debsign, /usr/sbin/pbuilder\" >> /etc/sudoers"
+sudo su -c "echo \"# $USER is given passwordless access to pbuilder and debsign for buildbot purposes.\" >> /etc/sudoers"
+sudo su -c "echo \"$USER    ALL=(ALL) NOPASSWD: SETENV: /usr/bin/debsign, /usr/sbin/pbuilder\" >> /etc/sudoers"
 
 sudo apt-get install pbuilder debootstrap devscripts
 cp pbuilderrc ~/.pbuilderrc
