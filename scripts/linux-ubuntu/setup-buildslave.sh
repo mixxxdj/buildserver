409b191aa66b82b443c94d44ce3330f29afcd57e
RJ Ryan <rryan@mixxx.org>
2012-12-13 14:25:29 -0500

    Remove echo in setup-buildslave.sh

--- scripts/linux-ubuntu/setup-buildslave.sh
+++ scripts/linux-ubuntu/setup-buildslave.sh
@@ -8,15 +8,15 @@ source $PROGDIR/config.sh
 sudo apt-get install bzr emacs build-essential
 
 # Enable passwordless sudo
-echo "WARNING: Enabling passwordless sudo acess for $USER."
-sudo su -c "echo \"# $USER is given passwordless access to pbuilder and debsign for buildbot purposes.\" >> /etc/sudoers"
-sudo su -c "echo \"$USER    ALL=(ALL) NOPASSWD: SETENV: /usr/bin/debsign, /usr/sbin/pbuilder\" >> /etc/sudoers"
+#echo "WARNING: Enabling passwordless sudo acess for $USER."
+#sudo su -c "echo \"# $USER is given passwordless access to pbuilder and debsign for buildbot purposes.\" >> /etc/sudoers"
+#sudo su -c "echo \"$USER    ALL=(ALL) NOPASSWD: SETENV: /usr/bin/debsign, /usr/sbin/pbuilder\" >> /etc/sudoers"
 
-echo sudo apt-get install pbuilder debootstrap devscripts
-echo cp pbuilderrc ~/.pbuilderrc
+sudo apt-get install pbuilder debootstrap devscripts
+cp pbuilderrc ~/.pbuilderrc
 
-for DIST in $DISTS; do 
-    for ARCH in $ARCHS; do 
-	echo sudo DIST=$DIST ARCH=$ARCH pbuilder create 
-    done; 
+for DIST in $DISTS; do
+    for ARCH in $ARCHS; do
+	sudo DIST=$DIST ARCH=$ARCH pbuilder create
+    done;
 done;
