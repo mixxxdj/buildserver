e6cc41d440960ad3810da7e29add3dce758c6418
RJ Ryan <rryan@mixxx.org>
2014-06-14 23:26:30 -0400

    Add script to build libmad.

---
+++ build_libmad.bat
@@ -0,0 +1,24 @@
+echo "Building libmad"
+set MAD_PATH=libmad-0.15.1b
+
+if %MACHINE_X86% (
+  set PLATFORM=Win32
+) else (
+  set PLATFORM=x64
+)
+
+if %CONFIG_RELEASE% (
+  set CONFIG=Release
+) else (
+  set CONFIG=Debug
+)
+
+cd build\%MAD_PATH%\msvc++
+%MSBUILD% libmad.sln /p:Configuration=%CONFIG% /p:Platform=%PLATFORM% /t:libmad:Clean;libmad:Rebuild
+
+REM TODO(rryan): Currently generating a static library only.
+copy %PLATFORM%\%CONFIG%\libmad.lib %LIB_DIR%
+REM copy %PLATFORM%\%CONFIG%\libmad.dll %LIB_DIR%
+copy ..\mad.h %INCLUDE_DIR% 
+
+cd %ROOT_DIR%
