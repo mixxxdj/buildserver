b46ff4466e49a09d32236d89dd96173899f19410
RJ Ryan <rryan@mixxx.org>
2014-09-07 01:47:26 -0400

    Add script to build libsndfile.
    Also, call vcvarsall.bat from build_environment.bat to fix an msbuild issue with 64-bit compilation in the libsndfile solution.

--- build_environment.bat
+++ build_environment.bat
@@ -22,10 +22,20 @@ SET LIB_DIR=%CD%\lib\
 SET INCLUDE_DIR=%CD%\include\
 SET BUILD_DIR=%CD%\build\
 
+if %MACHINE_X86% (
+  call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86
+) else (
+  call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86_amd64
+)
+
 call build_zlib.bat 
-
 call build_pthreads.bat
-
 call build_protobuf.bat
-call build_portmidi.batcall build_libid3tag.bat
+call build_portmidi.bat
+call build_libid3tag.bat
 call build_libmad.bat
+call build_libogg.bat
+call build_libvorbis.bat
+call build_libshout.bat
+call build_libflac.bat
+call build_libsndfile.bat
---
+++ build_libsndfile.bat
@@ -0,0 +1,23 @@
+echo "Building libsndfile"
+set SNDFILE_PATH=libsndfile-1.0.25
+
+if %MACHINE_X86% (
+  set PLATFORM=Win32
+) else (
+  set PLATFORM=x64
+)
+
+if %CONFIG_RELEASE% (
+  set CONFIG=Release
+) else (
+  set CONFIG=Debug
+)
+
+cd build\%SNDFILE_PATH%
+%MSBUILD% libsndfile.sln /p:Configuration=%CONFIG% /p:Platform=%PLATFORM% /t:libsndfile:Clean;libsndfile:Rebuild
+
+copy %PLATFORM%\%CONFIG%\libsndfile.lib %LIB_DIR%
+copy %PLATFORM%\%CONFIG%\libsndfile.dll %LIB_DIR%
+copy src\sndfile.h %INCLUDE_DIR%
+
+cd %ROOT_DIR%
