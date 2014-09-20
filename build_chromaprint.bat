05ac6465b7f2c896505ab72145cfc314219e9289
kain88-de <max_linke@gmx.de>
2014-09-20 22:39:02 +0200

    delete chromaprint solution and build it on the fly

--- build_chromaprint.bat
+++ build_chromaprint.bat
@@ -3,8 +3,10 @@ set CHROMAPRINT_PATH=chromaprint-1.1
 
 if %MACHINE_X86% (
   set PLATFORM=Win32
+  set CMAKE_CONF="Visual Studio 12 2013"
 ) else (
   set PLATFORM=x64
+  set CMAKE_CONF="Visual Studio 12 2013 Win64"
 )
 
 if %CONFIG_RELEASE% (
@@ -13,10 +15,8 @@ if %CONFIG_RELEASE% (
   set CONFIG=Debug
 )
 
-REM NOTE(rryan): generated solution with
-REM C:\mixxx\environments\prototype\build\chromaprint-1.1>cmake . -G "Visual Studio 12 2013" -DWITH_FFTW3=ON -DFFTW3_FFTW_LIBRARY=c:/mixxx/environments/prototype/lib/libfftw-3.3.dll -DWITH_AVFFT=OFF -DBUILD_EXAMPLES=OFF
-
 cd build\%CHROMAPRINT_PATH%
+cmake . -G %CMAKE_CONF% -DWITH_FFTW3=ON -DFFTW3_FFTW_LIBRARY=%LIB_DIR%/libfftw-3.3.dll -DWITH_AVFFT=OFF -DBUILD_EXAMPLES=OFF -DFFTW3_DIR=../../include
 %MSBUILD% chromaprint.sln /p:Configuration=%CONFIG% /p:Platform=%PLATFORM% /t:chromaprint:Clean;chromaprint:Rebuild
 
 copy %PLATFORM%\%CONFIG%\chromaprint.lib %LIB_DIR%
