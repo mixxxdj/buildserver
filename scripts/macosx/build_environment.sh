674d4ee19921766a47be1da10e066dc768b890eb
RJ Ryan <rryan@mixxx.org>
2014-09-26 14:41:10 -0400

    Build opus/opusfile and SQLite.

--- scripts/macosx/build_environment.sh
+++ scripts/macosx/build_environment.sh
@@ -96,11 +96,13 @@ $PROGDIR/build_chromaprint.sh
 $PROGDIR/build_flac.sh
 $PROGDIR/build_hss1394.sh
 $PROGDIR/build_ogg.sh
+$PROGDIR/build_opus.sh # depends on ogg
 $PROGDIR/build_portaudio.sh
 $PROGDIR/build_portmidi.sh
 $PROGDIR/build_protobuf.sh
 $PROGDIR/build_rubberband.sh
-$PROGDIR/build_qt4.sh
+$PROGDIR/build_sqlite.sh
+$PROGDIR/build_qt4.sh # depends on sqlite
 $PROGDIR/build_sndfile.sh
 $PROGDIR/build_taglib.sh
 $PROGDIR/build_vorbis.sh
