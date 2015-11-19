3af66c89d00d872d948759944f7f9f0e2d86517e
RJ Ryan <rryan@mixxx.org>
2015-11-18 21:24:55 -0800

    Add __declspec(dllexport) to all shout.h exported methods on Windows.

--- build/libshout-2.4.0/include/shout/shout.h
+++ build/libshout-2.4.0/include/shout/shout.h
@@ -27,6 +27,12 @@
 #include <os.h>
 #endif
 
+#if defined(WIN32)
+#define SHOUT_EXPORT __declspec(dllexport)
+#else
+#define SHOUT_EXPORT
+#endif
+
 #define SHOUTERR_SUCCESS		(0) /* No error */
 #define SHOUTERR_INSANE			(-1) /* Nonsensical arguments e.g. self being NULL */ 
 #define SHOUTERR_NOCONNECT		(-2) /* Couldn't connect */
@@ -83,60 +89,60 @@ extern "C" {
 #endif
 
 /* initializes the shout library. Must be called before anything else */
-void shout_init(void);
+SHOUT_EXPORT void shout_init(void);
 
 /* shuts down the shout library, deallocating any global storage. Don't call
  * anything afterwards */
-void shout_shutdown(void);
+SHOUT_EXPORT void shout_shutdown(void);
 
 /* returns a static version string.  Non-null parameters will be set to the
  * value of the library major, minor, and patch levels, respectively */
-const char *shout_version(int *major, int *minor, int *patch);
+SHOUT_EXPORT const char *shout_version(int *major, int *minor, int *patch);
 
 /* Allocates and sets up a new shout_t.  Returns NULL if it can't get enough
  * memory.  The returns shout_t must be disposed of with shout_free. */
-shout_t *shout_new(void);
+SHOUT_EXPORT shout_t *shout_new(void);
 
 /* Free all memory allocated by a shout_t */
-void shout_free(shout_t *self);
+SHOUT_EXPORT void shout_free(shout_t *self);
 
 /* Returns a statically allocated string describing the last shout error
  * to occur.  Only valid until the next libshout call on this shout_t */
-const char *shout_get_error(shout_t *self);
+SHOUT_EXPORT const char *shout_get_error(shout_t *self);
 
 /* Return the error code (e.g. SHOUTERR_SOCKET) for this shout instance */
-int shout_get_errno(shout_t *self);
+SHOUT_EXPORT int shout_get_errno(shout_t *self);
 
 /* returns SHOUTERR_CONNECTED or SHOUTERR_UNCONNECTED */
-int shout_get_connected(shout_t *self);
+SHOUT_EXPORT int shout_get_connected(shout_t *self);
 
 /* Parameter manipulation functions.  libshout makes copies of all parameters,
  * the caller may free its copies after giving them to libshout.  May return
  * SHOUTERR_MALLOC */
 
 /* Connection parameters */
-int shout_set_host(shout_t *self, const char *host);
-const char *shout_get_host(shout_t *self);
+SHOUT_EXPORT int shout_set_host(shout_t *self, const char *host);
+SHOUT_EXPORT const char *shout_get_host(shout_t *self);
 
-int shout_set_port(shout_t *self, unsigned short port);
-unsigned short shout_get_port(shout_t *self);
+SHOUT_EXPORT int shout_set_port(shout_t *self, unsigned short port);
+SHOUT_EXPORT unsigned short shout_get_port(shout_t *self);
 
-int shout_set_agent(shout_t *self, const char *agent);
-const char *shout_get_agent(shout_t *self);
+SHOUT_EXPORT int shout_set_agent(shout_t *self, const char *agent);
+SHOUT_EXPORT const char *shout_get_agent(shout_t *self);
 
 /* See SHOUT_TLS_* above */
-int shout_set_tls(shout_t *self, int mode);
-int shout_get_tls(shout_t *self);
+SHOUT_EXPORT int shout_set_tls(shout_t *self, int mode);
+SHOUT_EXPORT int shout_get_tls(shout_t *self);
 
 /* Set the directory for CA certs. Default: operating system's default */
-int shout_set_ca_directory(shout_t *self, const char *directory);
-const char *shout_get_ca_directory(shout_t *self);
+SHOUT_EXPORT int shout_set_ca_directory(shout_t *self, const char *directory);
+SHOUT_EXPORT const char *shout_get_ca_directory(shout_t *self);
 
 /* Set a CA cert file for checking. If you use a self signed server cert
  * you can pass this cert using this function for verification.
  * Default: operating system's default */
-int shout_set_ca_file(shout_t *self, const char *file);
-const char *shout_get_ca_file(shout_t *self);
+SHOUT_EXPORT int shout_set_ca_file(shout_t *self, const char *file);
+SHOUT_EXPORT const char *shout_get_ca_file(shout_t *self);
 
 /* Set list of allowed ciphers.
  * This function should only be used in case of using an old libshout
@@ -145,89 +151,89 @@ const char *shout_get_ca_file(shout_t *self);
  * DO NOT SET THIS TO ANY FIXED VALUE. IF YOU USE THIS FUNCTION
  * EXPOSE IT TO THE USER. OTHERWISE YOU WILL HARM SECURITY.
  * Default: internal list of secure ciphers. */
-int shout_set_allowed_ciphers(shout_t *self, const char *ciphers);
-const char *shout_get_allowed_ciphers(shout_t *self);
+SHOUT_EXPORT int shout_set_allowed_ciphers(shout_t *self, const char *ciphers);
+SHOUT_EXPORT const char *shout_get_allowed_ciphers(shout_t *self);
 
 /* Authentication parameters */
-int shout_set_user(shout_t *self, const char *username);
-const char *shout_get_user(shout_t *self);
+SHOUT_EXPORT int shout_set_user(shout_t *self, const char *username);
+SHOUT_EXPORT const char *shout_get_user(shout_t *self);
 
-int shout_set_password(shout_t *, const char *password);
-const char *shout_get_password(shout_t *self);
+SHOUT_EXPORT int shout_set_password(shout_t *, const char *password);
+SHOUT_EXPORT const char *shout_get_password(shout_t *self);
 
 /* Set a client certificate for TLS connections.
  * This must be in PEM format with both cert and private key in the same file.
  * Default: none. */
-int shout_set_client_certificate(shout_t *self, const char *certificate);
-const char *shout_get_client_certificate(shout_t *self);
+SHOUT_EXPORT int shout_set_client_certificate(shout_t *self, const char *certificate);
+SHOUT_EXPORT const char *shout_get_client_certificate(shout_t *self);
 
 /* Mount parameters */
-int shout_set_mount(shout_t *self, const char *mount);
-const char *shout_get_mount(shout_t *self);
+SHOUT_EXPORT int shout_set_mount(shout_t *self, const char *mount);
+SHOUT_EXPORT const char *shout_get_mount(shout_t *self);
 
 /* Other parameters */
-int shout_set_name(shout_t *self, const char *name); // obsolete
-const char *shout_get_name(shout_t *self); // obsolete
+SHOUT_EXPORT int shout_set_name(shout_t *self, const char *name); // obsolete
+SHOUT_EXPORT const char *shout_get_name(shout_t *self); // obsolete
 
-int shout_set_url(shout_t *self, const char *url); // obsolete
-const char *shout_get_url(shout_t *self); // obsolete
+SHOUT_EXPORT int shout_set_url(shout_t *self, const char *url); // obsolete
+SHOUT_EXPORT const char *shout_get_url(shout_t *self); // obsolete
 
-int shout_set_genre(shout_t *self, const char *genre); // obsolete
-const char *shout_get_genre(shout_t *self); // obsolete
+SHOUT_EXPORT int shout_set_genre(shout_t *self, const char *genre); // obsolete
+SHOUT_EXPORT const char *shout_get_genre(shout_t *self); // obsolete
 
-int shout_set_description(shout_t *self, const char *description); // obsolete
-const char *shout_get_description(shout_t *self); // obsolete
+SHOUT_EXPORT int shout_set_description(shout_t *self, const char *description); // obsolete
+SHOUT_EXPORT const char *shout_get_description(shout_t *self); // obsolete
 
-int shout_set_dumpfile(shout_t *self, const char *dumpfile);
-const char *shout_get_dumpfile(shout_t *self);
+SHOUT_EXPORT int shout_set_dumpfile(shout_t *self, const char *dumpfile);
+SHOUT_EXPORT const char *shout_get_dumpfile(shout_t *self);
 
-int shout_set_audio_info(shout_t *self, const char *name, const char *value);
-const char *shout_get_audio_info(shout_t *self, const char *name);
+SHOUT_EXPORT int shout_set_audio_info(shout_t *self, const char *name, const char *value);
+SHOUT_EXPORT const char *shout_get_audio_info(shout_t *self, const char *name);
 
 /* takes a SHOUT_META_xxxx argument */
-int shout_set_meta(shout_t *self, const char *name, const char *value);
-const char *shout_get_meta(shout_t *self, const char *name);
+SHOUT_EXPORT int shout_set_meta(shout_t *self, const char *name, const char *value);
+SHOUT_EXPORT const char *shout_get_meta(shout_t *self, const char *name);
 
-int shout_set_public(shout_t *self, unsigned int make_public);
-unsigned int shout_get_public(shout_t *self);
+SHOUT_EXPORT int shout_set_public(shout_t *self, unsigned int make_public);
+SHOUT_EXPORT unsigned int shout_get_public(shout_t *self);
 
 /* takes a SHOUT_FORMAT_xxxx argument */
-int shout_set_format(shout_t *self, unsigned int format);
-unsigned int shout_get_format(shout_t *self);
+SHOUT_EXPORT int shout_set_format(shout_t *self, unsigned int format);
+SHOUT_EXPORT unsigned int shout_get_format(shout_t *self);
 
 /* takes a SHOUT_PROTOCOL_xxxxx argument */
-int shout_set_protocol(shout_t *self, unsigned int protocol);
-unsigned int shout_get_protocol(shout_t *self);
+SHOUT_EXPORT int shout_set_protocol(shout_t *self, unsigned int protocol);
+SHOUT_EXPORT unsigned int shout_get_protocol(shout_t *self);
 
 /* Instructs libshout to use nonblocking I/O. Must be called before
  * shout_open (no switching back and forth midstream at the moment). */
-int shout_set_nonblocking(shout_t* self, unsigned int nonblocking);
-unsigned int shout_get_nonblocking(shout_t *self);
+SHOUT_EXPORT int shout_set_nonblocking(shout_t* self, unsigned int nonblocking);
+SHOUT_EXPORT unsigned int shout_get_nonblocking(shout_t *self);
 
 /* Opens a connection to the server.  All parameters must already be set */
-int shout_open(shout_t *self);
+SHOUT_EXPORT int shout_open(shout_t *self);
 
 /* Closes a connection to the server */
-int shout_close(shout_t *self);
+SHOUT_EXPORT int shout_close(shout_t *self);
 
 /* Send data to the server, parsing it for format specific timing info */
-int shout_send(shout_t *self, const unsigned char *data, size_t len);
+SHOUT_EXPORT int shout_send(shout_t *self, const unsigned char *data, size_t len);
 
 /* Send unparsed data to the server.  Do not use this unless you know
  * what you are doing. 
  * Returns the number of bytes written, or < 0 on error.
  */
-ssize_t shout_send_raw(shout_t *self, const unsigned char *data, size_t len);
+SHOUT_EXPORT ssize_t shout_send_raw(shout_t *self, const unsigned char *data, size_t len);
 
 /* return the number of bytes currently on the write queue (only makes sense in
  * nonblocking mode). */
-ssize_t shout_queuelen(shout_t *self);
-  
+SHOUT_EXPORT ssize_t shout_queuelen(shout_t *self);
+
 /* Puts caller to sleep until it is time to send more data to the server */
-void shout_sync(shout_t *self);
+SHOUT_EXPORT void shout_sync(shout_t *self);
 
 /* Amount of time in ms caller should wait before sending again */
-int shout_delay(shout_t *self);
+SHOUT_EXPORT int shout_delay(shout_t *self);
 
 /* Sets MP3 metadata.
  * Returns:
@@ -238,20 +244,20 @@ int shout_delay(shout_t *self);
  *   SHOUTERR_NOCONNECT
  *   SHOUTERR_SOCKET
  */
-int shout_set_metadata(shout_t *self, shout_metadata_t *metadata);
+SHOUT_EXPORT int shout_set_metadata(shout_t *self, shout_metadata_t *metadata);
 
 /* Allocates a new metadata structure.  Must be freed by shout_metadata_free. */
-shout_metadata_t *shout_metadata_new(void);
+SHOUT_EXPORT shout_metadata_t *shout_metadata_new(void);
 
 /* Free resources allocated by shout_metadata_t */
-void shout_metadata_free(shout_metadata_t *self);
+SHOUT_EXPORT void shout_metadata_free(shout_metadata_t *self);
 
 /* Add a parameter to the metadata structure.
  * Returns:
  *   SHOUTERR_SUCCESS on success
  *   SHOUTERR_INSANE if self isn't a valid shout_metadata_t* or name is null
  *   SHOUTERR_MALLOC if memory can't be allocated */
-int shout_metadata_add(shout_metadata_t *self, const char *name, const char *value);
+SHOUT_EXPORT int shout_metadata_add(shout_metadata_t *self, const char *name, const char *value);
 
 #ifdef __cplusplus
 }
@@ -259,7 +265,7 @@ int shout_metadata_add(shout_metadata_t *self, const char *name, const char *val
 
 /* --- Compiled features --- */
 
-#define SHOUT_THREADSAFE @SHOUT_THREADSAFE@
-#define SHOUT_TLS        @SHOUT_TLS@
+#define SHOUT_THREADSAFE 1
+#define SHOUT_TLS        0
 
 #endif /* __LIBSHOUT_SHOUT_H__ */
