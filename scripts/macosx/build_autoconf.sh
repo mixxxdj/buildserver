3f927e7716c87f9b7545ee340d1a6afa415f4f77
RJ Ryan <rryan@mixxx.org>
2016-05-24 12:33:50 -0700

    Add autoconf/automake/libtool build scripts.

---
+++ scripts/macosx/build_autoconf.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Echo commands
+set -x
+
+# Quit on errors
+set -e
+
+# make sure to get the path right
+pushd `dirname $0` > /dev/null
+PROGDIR=`pwd -P`
+popd > /dev/null
+
+export VERSION_NUMBER=2.69
+export VERSION=autoconf-${VERSION_NUMBER}
+export ARCHIVE=$VERSION.tar.xz
+
+tar -zxf $DEPENDENCIES/$ARCHIVE
+cd $VERSION
+
+source $PROGDIR/environment.sh
+
+./configure --prefix=$MIXXX_PREFIX && make && make install
