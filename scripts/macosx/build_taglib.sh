de1ecb4099b47d14111c3f7c182ce9f62050c4c2
RJ Ryan <rryan@mixxx.org>
2016-05-24 12:30:33 -0700

    Bump taglib to 1.11 (and update old/deprecated flags).

--- scripts/macosx/build_taglib.sh
+++ scripts/macosx/build_taglib.sh
@@ -11,14 +11,14 @@ pushd `dirname $0` > /dev/null
 PROGDIR=`pwd -P`
 popd > /dev/null
 
-export VERSION_NUMBER=1.10
+export VERSION_NUMBER=1.11
 export VERSION=taglib-${VERSION_NUMBER}
 export ARCHIVE=$VERSION.tar.gz
 
 echo "Building $VERSION for $MIXXX_ENVIRONMENT_NAME for architectures: ${MIXXX_ARCHS[@]}"
 
 # You may need to change these from version to version.
-export DYLIB=taglib/libtag.1.15.1.dylib
+export DYLIB=taglib/libtag.1.16.0.dylib
 export STATICLIB=taglib/libtag.a
 
 for ARCH in ${MIXXX_ARCHS[@]}
@@ -28,13 +28,13 @@ do
   cd $VERSION-$ARCH
   source $PROGDIR/environment.sh $ARCH
   # To build static, use -DENABLE_STATIC=ON but this turns off building a shared library.
-  cmake . -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$MIXXX_PREFIX" -DCMAKE_OSX_DEPLOYMENT_TARGET="$MACOSX_DEPLOYMENT_TARGET" -DCMAKE_OSX_SYSROOT="$OSX_SDK" -DCMAKE_VERBOSE_MAKEFILE=TRUE -DWITH_ASF=ON -DWITH_MP4=ON
+  cmake . -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$MIXXX_PREFIX" -DCMAKE_OSX_DEPLOYMENT_TARGET="$MACOSX_DEPLOYMENT_TARGET" -DCMAKE_OSX_SYSROOT="$OSX_SDK" -DCMAKE_VERBOSE_MAKEFILE=TRUE -DBUILD_SHARED_LIBS=TRUE
   make
   cd ..
 done
 
-# Install the i386 version in case there are binaries we want to run (our host is i386)
-export ARCH=i386
+# Install the host version in case there are binaries we want to run.
+export ARCH=$HOST_ARCH
 cd $VERSION-$ARCH
 source $PROGDIR/environment.sh $ARCH
 
