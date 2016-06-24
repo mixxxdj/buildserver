a820c8e3a286dde1ffa1ab33bbe6567a1190d793
Sean M. Pappalardo <spappalardo@mixxx.org>
2016-06-24 01:22:42 -0400

    Correct FFTW script handling of static vs dynamic builds. Add build of libfftwf for static builds.

--- build_fftw3.bat
+++ build_fftw3.bat
@@ -8,23 +8,38 @@ if %MACHINE_X86% (
 )
 
 if %CONFIG_RELEASE% (
-  set CONFIG=Static-Release
+  set CONFIG=Release
+  if %STATIC_LIBS% ( 
+    set CONFIG=Static-Release
+  )
 ) else (
-  set CONFIG=Static-Debug
+  set CONFIG=Debug
+  if %STATIC_LIBS% ( 
+    set CONFIG=Static-Debug
+  )
 )
 
-rem cd build\%FFTW_PATH%\fftw-3.3-libs
-cd build\%FFTW_PATH%\fftw-3.3-libs\libfftw-3.3
+cd build\%FFTW_PATH%\fftw-3.3-libs
+rem cd build\%FFTW_PATH%\fftw-3.3-libs\
 
-REM TODO(Pegasus): Check for %STATIC_LIBS% and if not set, define FFTW_DLL somehow.
+echo Cleaning all...
+%MSBUILD% fftw-3.3-libs.sln /p:Configuration=%CONFIG% /p:Platform=%PLATFORM% /t:Clean
 
 REM NOTE(rryan): Couldn't get solution itself to resolve the build actions. Odd.
-rem %MSBUILD% fftw-3.3-libs.sln /p:Configuration=%CONFIG% /p:Platform=%PLATFORM% /t:libfftw-3.3:Clean;libfftw-3.3:Rebuild
-%MSBUILD% libfftw-3.3.vcxproj /p:Configuration=%CONFIG% /p:Platform=%PLATFORM% /t:Clean;Rebuild
-
+cd libfftw-3.3
+%MSBUILD% libfftw-3.3.vcxproj /p:Configuration=%CONFIG% /p:Platform=%PLATFORM% /t:Rebuild
 copy %PLATFORM%\%CONFIG%\libfftw-3.3.lib %LIB_DIR%
 copy %PLATFORM%\%CONFIG%\libfftw-3.3.pdb %LIB_DIR%
-copy %PLATFORM%\%CONFIG%\libfftw-3.3.dll %LIB_DIR%
+if not %STATIC_LIBS% ( 
+  copy %PLATFORM%\%CONFIG%\libfftw-3.3.dll %LIB_DIR%
+) else (
+  rem Need libfftwf as well
+  cd ..\libfftwf-3.3
+  %MSBUILD% libfftwf-3.3.vcxproj /p:Configuration=%CONFIG% /p:Platform=%PLATFORM% /t:Rebuild
+  copy %PLATFORM%\%CONFIG%\libfftwf-3.3.lib %LIB_DIR%
+  copy %PLATFORM%\%CONFIG%\libfftwf-3.3.pdb %LIB_DIR%
+)
+
 copy ..\..\api\fftw3.h %INCLUDE_DIR%
 
 cd %ROOT_DIR%
