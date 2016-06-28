c63562bfa5fce0d4a042226973adc84eb2986dd1
Sean M. Pappalardo <pegasus@renegadetech.com>
2016-06-27 20:41:31 -0400

    Enhanced build_chromaprint to re-generate the VC project files and build the static one when appropriate.

--- build_chromaprint.bat
+++ build_chromaprint.bat
@@ -1,10 +1,14 @@
-echo "Building chromaprint"
+echo ---- Building chromaprint ----
 set CHROMAPRINT_PATH=chromaprint-1.3.1
 
 if %MACHINE_X86% (
   set PLATFORM=Win32
+  rem No space after = below!
+  set CMAKE_PLATFORM=
 ) else (
   set PLATFORM=x64
+  rem Space between = and Win64 is intentional and required!
+  set CMAKE_PLATFORM= Win64
 )
 
 if %CONFIG_RELEASE% (
@@ -13,15 +17,28 @@ if %CONFIG_RELEASE% (
   set CONFIG=Debug
 )
 
-REM NOTE(pegasus): generated solution one with
-REM cmake . -G "Visual Studio 14 2015 Win64" -DWITH_FFTW3=ON -DFFTW3_DIR=../..
+REM Remove any existing files
+erase %LIB_DIR%\*chromaprint*
 
 cd build\%CHROMAPRINT_PATH%
-%MSBUILD% chromaprint.sln /p:Configuration=%CONFIG% /p:Platform=%PLATFORM% /t:chromaprint:Clean;chromaprint:Rebuild
+REM Re-generate solution files.
+erase cmakecache.txt
+REM If CMAKE_PLATFORM is empty, CMake generates a Win32 version.
+REM   No separating space is intentional
+cmake . -G "Visual Studio 14 2015%CMAKE_PLATFORM%" -DWITH_FFTW3=ON -DFFTW3_DIR=../..
 
-copy %PLATFORM%\%CONFIG%\chromaprint.lib %LIB_DIR%
-copy %PLATFORM%\%CONFIG%\chromaprint.dll %LIB_DIR%
-copy %PLATFORM%\%CONFIG%\chromaprint.pdb %LIB_DIR%
+IF %STATIC_LIBS% (
+  set PROJECT=chromaprint_p
+) else (
+  set PROJECT=chromaprint
+)
+
+%MSBUILD% chromaprint.sln /p:Configuration=%CONFIG% /p:Platform=%PLATFORM% /t:Clean;%PROJECT%:Rebuild
+
+copy src\%CONFIG%\%PROJECT%.lib %LIB_DIR%
+copy src\%CONFIG%\%PROJECT%.dll %LIB_DIR%
+copy src\%PROJECT%.dir\%CONFIG%\%PROJECT%.pdb %LIB_DIR%
+
 copy src\chromaprint.h %INCLUDE_DIR%
 
 cd %ROOT_DIR%
