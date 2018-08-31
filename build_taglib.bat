729dc209e64744246f7650cb7fccacc23f58d8d8
RJ Ryan <rryan@mixxx.org>
2018-08-30 19:02:55 -0700

    Fix taglib-1.11 PDB copy path. Leftover from #58.

--- build_taglib.bat
+++ build_taglib.bat
@@ -26,31 +26,30 @@ if %STATIC_LIBS% (
 )
 
 cd build\%TAGLIB_PATH%
-REM Re-generate solution files.
+
 rd /S /Q %PLATFORM%
 mkdir %PLATFORM%
 cd %PLATFORM%
-REM NOTE(pegasus): generated solution with
 
 "%CMAKEDIR%\cmake" .. -G %COMPILERPROJECT% -T %PLATFORM_TOOLSET% -DBUILD_SHARED_LIBS=%DYNAMIC% -DCMAKE_SUPPRESS_REGENERATION=1 -DZLIB_INCLUDE_DIR=%INCLUDE_DIR% -DZLIB_LIBRARY=%LIB_DIR%\%ZLIBNAME% -DCMAKE_INSTALL_PREFIX=%ROOT_DIR%
 IF ERRORLEVEL 1 (
     SET VALRETURN=1
 	goto END
 )
- 
-"%CMAKEDIR%\cmake" --build . --config %CONFIG% --clean-first 
+
+"%CMAKEDIR%\cmake" --build . --config %CONFIG% --clean-first
 IF ERRORLEVEL 1 (
     SET VALRETURN=1
 	goto END
 )
 
-"%CMAKEDIR%\cmake" --build . --config %CONFIG% --target install 
+"%CMAKEDIR%\cmake" --build . --config %CONFIG% --target install
 IF ERRORLEVEL 1 (
     SET VALRETURN=1
     goto END
 )
 
-copy tag.dir\RelWithDebInfo\tag.pdb %LIB_DIR%
+copy taglib\tag.dir\%CONFIG%\tag.pdb %LIB_DIR%
 
 :END
 cd %ROOT_DIR%
