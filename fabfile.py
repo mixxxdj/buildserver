8db41ebd251303ff7575226a6a907a2ab4458c4a
Daniel Schürmann <daschuer@mixxx.org>
2018-09-16 23:02:54 +0200

    Added qtkeychain dev packeges ti fabfile.py

--- fabfile.py
+++ fabfile.py
@@ -41,6 +41,7 @@ MIXXX_DEBIAN_DEPENDENCIES = [
     'libqt4-dev',
     'libqt4-opengl-dev',
     'libqt4-sql-sqlite',
+	'qtkeychain-dev', # qt4 version 
     'libqt5opengl5-dev',
     'libqt5sql5-sqlite',
     'libqt5svg5-dev',
@@ -60,6 +61,7 @@ MIXXX_DEBIAN_DEPENDENCIES = [
     'qtdeclarative5-dev',
     'qtdeclarative5-dev-tools',
     'qtscript5-dev',
+	'qt5keychain-dev',
     'scons',
     'vamp-plugin-sdk',
 ]
