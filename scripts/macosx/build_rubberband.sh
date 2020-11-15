c379ab69eb3dcf059df0c0363ab441811fd2f988
Be <be@mixxx.org>
2020-11-14 23:20:57 -0600

    update rubberband to 1.9.0

--- scripts/macosx/build_rubberband.sh
+++ scripts/macosx/build_rubberband.sh
@@ -11,7 +11,7 @@ pushd `dirname $0` > /dev/null
 PROGDIR=`pwd -P`
 popd > /dev/null
 
-export VERSION_NUMBER=1.8.1
+export VERSION_NUMBER=1.9.0
 export VERSION=rubberband-${VERSION_NUMBER}
 export ARCHIVE=$VERSION.tar.bz2
 
