fe7e539ec3b636c9034d0c19dccc7203847addeb
Be <be@mixxx.org>
2020-11-14 23:11:24 -0600

    update libtool to 2.4.6

--- scripts/macosx/build_libtool.sh
+++ scripts/macosx/build_libtool.sh
@@ -11,7 +11,7 @@ pushd `dirname $0` > /dev/null
 PROGDIR=`pwd -P`
 popd > /dev/null
 
-export VERSION_NUMBER=2.4
+export VERSION_NUMBER=2.4.6
 export VERSION=libtool-${VERSION_NUMBER}
 export ARCHIVE=$VERSION.tar.xz
 
